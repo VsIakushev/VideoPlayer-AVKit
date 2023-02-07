39e1fd7fef6e9e13f5ab02504f753a8d91955468
Vitalii Iakushev <vsiakushev@gmail.com>
2023-02-07 21:37:44 +0400

    Initial Commit

---
+++ VideoPlayer AVKit/VideoPlayer_AVKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayer_AVKitApp.swift
+//  VideoPlayer AVKit
+//
+//  Created by Vitaliy Iakushev on 07.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayer_AVKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
